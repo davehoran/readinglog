7dbdd413aa3f95c84a4c5c6bd4abd32c39b7eb4d
davehoran <dave.horan@gmail.com>
2017-10-11 16:35:24 -0400

    buttons added to home page, and readBook() function added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { BooksPage } from '../books/books';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController) {
+
 
   }
-
+  readBook(){
+    console.log('read book clicked');
+  }
 }
