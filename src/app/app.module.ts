c13dceb7a8fa08ac1152cb2ebef409cfd34c663d
Dave Horan <dave.horan@gmail.com>
2017-10-18 21:58:46 -0400

    added settings page and reworked the tab navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { BookListPage } from '../pages/book-list/book-list';
 import { ReadBookPage } from '../pages/read-book/read-book';
 import { LoginPage } from '../pages/login/login';
 import { RegisterPage } from '../pages/register/register';
+import { ReportsPage } from '../pages/reports/reports';
+import { SettingsPage } from '../pages/settings/settings';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -19,13 +20,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
     TabsPage,
     BookListPage,
     ReadBookPage,
     LoginPage,
-    RegisterPage
+    RegisterPage,
+    ReportsPage,
+    SettingsPage
   ],
   imports: [
     BrowserModule,
@@ -35,13 +37,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
     TabsPage,
     BookListPage,
     ReadBookPage,
     LoginPage,
-    RegisterPage
+    RegisterPage,
+    ReportsPage,
+    SettingsPage
   ],
   providers: [
     StatusBar,
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { LoginPage } from '../login/login';
 import { HomePage } from '../home/home';
+import { BookListPage } from '../book-list/book-list';
+import { ReadBookPage } from '../read-book/read-book';
+import { ReportsPage } from '../reports/reports';
+import { SettingsPage } from '../settings/settings';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -10,8 +12,10 @@ import { HomePage } from '../home/home';
 export class TabsPage {
 
   tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = LoginPage;
+  tab2Root = BookListPage;
+  tab3Root = ReadBookPage;
+  tab4Root = ReportsPage;
+  tab5Root = SettingsPage;
 
   constructor() {
 
